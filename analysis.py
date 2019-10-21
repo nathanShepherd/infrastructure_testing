3f1527969f3f9f424723dc4e4e1dab409a2e3d22
Nathan Shepherd <nateshep@umich.edu>
2019-10-20 22:44:19 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,16 +1,15 @@
 from trex_archiver import collect_archive
+from math import sqrt
 
 def viz_simple_stats(simple_stats):
   for t in simple_stats:
     print(t)
     for tt in simple_stats[t]:
-      if t == "Total-tx-bytes":
-        tt = str(float(tt.split("bytes")[0]) / 10000000000) + " GB"
       if t == "CpuUtilization":
         tt = tt.split("Gb/core")[0].split("%")
         tt = tt[0] +" % "+ tt[1] + " Gb/core"
-
-      print("\t" + tt)
+       
+      print("\t" + str(tt))
 
       if tt in ["0", "1"]:
         for ttt in simple_stats[t][tt]:
@@ -18,22 +17,62 @@ def viz_simple_stats(simple_stats):
           for tttt in simple_stats[t][tt][ttt]:
             print("\t\t" + tttt)
 
-def clean_archive(archive):
-  print("***** CLEANED *****")
-  for test in archive:
-    for t in archive[test]:
-      print(t)
+def clean(archive):
+        outs = {}
+	for title in archive:
+		temp = {}
+		for test in archive[title]:
+			if test not in ["CpuUtilization", "ports"]:
+				temp[test] = archive[title][test]
+                outs[title] = temp
+	return outs
 
-      for tt in archive[test][t]:
-        if "port" not in tt:
-          break
-        print('\t' + tt)
+def mean(in_arr):
+	return sum(in_arr)/len(in_arr)
+
+def sigma(arr, mean):
+	summ = 0
+	for el in arr:
+		summ += (el - mean) ** 2
+	return sqrt(el)
+
+def title_stats(json):
+        outs = {}
+	for title in json:
+		outs[title] = {}
+		print(title) #########
+		for test in json[title]:
+                        outs[title][test] = {}
+                        entries = json[title][test]
+			print(entries)#########
+			if '' in entries:
+				continue
+
+                        mu = mean(entries)
+			outs[title][test]["mean"] = mu 
+			outs[title][test]["StdDev"] = sigma(entries, mu)
+			outs[title][test]["num"] = len(entries)
+
+	return outs
+			
+def print_gen_stats(archive):
+	for title in archive:
+		print("\n--> " + title)
+		for el in archive[title]:
+			print(el)
+			for stat in archive[title][el]:
+                                out = '\t' + stat + "\t" 
+				print(out + str(archive[title][el][stat]))
+				
 
 if __name__ == "__main__":
-        hive = {}
-        collect_archive(hive)
+	hive = {}
+	collect_archive(hive)
+        hive = clean(hive)
 
-        for title in hive:
+	for title in hive:
                 print("||| TITLE ||| ::: " + title)
-                viz_simple_stats(hive[title])
-        clean_archive(hive)
+		viz_simple_stats(hive[title])
+
+	hive_stat = title_stats(hive)
+        print_gen_stats(hive_stat)
