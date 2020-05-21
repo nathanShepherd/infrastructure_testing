2caf8fc12c4f98db385c7482c0d71f925692cbb8
Nathan Daniel Shepherd <nateshep@its-perfsonar-bastion.dsc.umich.edu>
2020-05-21 00:30:45 -0400

    Updates

--- test_automation.py
+++ test_automation.py
@@ -12,15 +12,22 @@ OUT = "/opt/trex/v2.57/test_dir/"
 # Dictionary T (test) defines each test including the associated executable
 # Usage: T[ testname ] returns TRex command for test type "testname"
 T = {"pS_thruput":"",
-     "initial_testing":_exe +"-f cap2/http_simple.yaml -c 4 -m 10 -d 1 -l 1000",
+	"initial_testing":_exe +"-f cap2/http_simple.yaml -c 4 -m 10 -d 1 -l 1000",
 
-     "imix_64_100k": _exe +"-f cap2/imix_64_100k.yaml -c 4 -m 2 -d 60 -l 1000",
+	"imix_64_100k": _exe +"-f cap2/imix_64_100k.yaml -c 6 -m 2 -d 1 -l 1000",
 
-     no_bundle:_exe +"-f avl/"+ no_bundle +".yaml -c 4 -m 20 -d 60 -l 1000 --ipv6",
+	no_bundle:_exe +"-f avl/"+ no_bundle +".yaml -c 6 -m 20 -d 1 -l 1000 --ipv6",
 
-     "sfr_delay_10_1g":_exe+"-f avl/sfr_delay_10_1g.yaml -c 4 -m 35 -d 100 -l 1000 -p",
 
-     "http_simple": _exe +"-f cap2/http_simple.yaml -c 4 -m 100 -d 1 -l 1000"}
+	"sfr_delay_10_1g":_exe+"-f avl/sfr_delay_10_1g.yaml -c 4 -m 35 -d 100 -l 1000 -p",
+	"sfr_delay_10_1g_6cores":_exe+"-f avl/sfr_delay_10_1g.yaml -c 6 -m 21 -d 100 -l 1000 -p",
+
+	"many_clients": "./t-rex-64 -f cap2/many_client_example.yaml -c 6 -m 1 -d 3 -l 1000",
+	"http_both_ports_client": _exe +"-f cap2/http_simple.yaml -c 6 -m 100 -d 1 -l 1000 -p",
+
+	"http_simple": _exe +"-f cap2/http_simple.yaml -c 4 -m 100 -d 1 -l 1000",
+	"http_6cores": _exe +"-f cap2/http_simple.yaml -c 6 -m 100 -d 1 -l 1000",
+	}
 
 def link(title):
         ''' Execute one test given the title definition '''
@@ -48,12 +55,13 @@ def get_multiplier_data_pts(title):
 	out = OUT + dir_name
 	system("mkdir " + out)
 
-        max_multi = 200
-	step_multi = 10
-	num_test = 3
-	ts = time()
+        max_multi = 1000011
+	step_multi = 100001
+	start_multi = max(1, 1)
+	num_test = 1
+	ts = time()# start of multi_test
 
-	for m in range(1, max_multi, step_multi):
+	for m in range(start_multi, max_multi, step_multi):
 		sub_dir = out +'/'+ 'm' + str(m)
 		
 		system("mkdir " + sub_dir)
@@ -62,9 +70,10 @@ def get_multiplier_data_pts(title):
                         test_start = time()
 			stamp = '_' + timestmp(test_start).strftime("%d%b%Y_%Hh%Mm%Ss")
 			filename = sub_dir +'/'+ title + stamp
-			system("touch " + filename)
+			system("touch " + filename)			
 			
-			print(filename)
+			print("Created file: " + title + stamp)
+			
 
 			# remove any number after -m, replace with multiplier for this test
 			_test = T[title].split("-m")
@@ -74,18 +83,22 @@ def get_multiplier_data_pts(title):
 
 			print("Stored multi test " + str(file) + " of " + str(num_test))
 			if file == 0:
-				t_elapsed = time() - ts
-				print("duration of test " + str(t_elapsed))
-				t_remain = t_elapsed * num_test * ((max_multi - 1) / step_multi)
+
+				t_elapsed = time() - test_start
+				print("duration of last test " + str(t_elapsed/60) + " min")
+
+				t_remain = t_elapsed * num_test
+				t_remain *= ((max_multi - m - start_multi) / step_multi)
 				t_remain = round(t_remain/60, 1)
 				t_remain = str(t_remain) +" minutes"
 				print("Estimated time remain "+ t_remain)
-		print("Completed m = " + str(m) + ", in time " + str((time() - ts)/60) + "min")
-	print("All multi tests complete in " + str((time() - ts)/60) )
+
+		print("Completed m = "+ str(m) +", in time "+ str((time() - test_start)/60) +" min")
+	print("All multi tests complete in " + str((time() - ts)/60) +" minutes")
 		
 	
 
 if __name__ == "__main__":
 	#link("http_simple")
-	get_multiplier_data_pts("http_simple")
+	get_multiplier_data_pts('http_both_ports_client')
 
